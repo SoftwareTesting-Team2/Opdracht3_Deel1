a0a681e82f80798d7337b6ea0cbdd050b30688d7
TiboMenardo <91217390+TiboMenardo@users.noreply.github.com>
2023-11-17 09:11:13 +0100

    cucumber GUI gebruiken

--- src/test/java/be/odisee/brainstormOnderwerp/acceptancetests/steps/DemoInplannenStepDefinitions.java
+++ src/test/java/be/odisee/brainstormOnderwerp/acceptancetests/steps/DemoInplannenStepDefinitions.java
@@ -15,38 +15,46 @@ public class DemoInplannenStepDefinitions {
 
 
     @Gegeven("^er is een demo aangemaakt$")
-    public void er_is_een_demo_aangemaakt() {
+    public void er_is_een_demo_aangemaakt()
+    {
         demo = new Demo();
     }
 
     @Gegeven("^er is een ambassadeur geselecteerd voor de demo$")
-    public void er_is_een_ambassadeur_geselecteerd_voor_de_demo() {
+    public void er_is_een_ambassadeur_geselecteerd_voor_de_demo()
+    {
         demo.setAmbassadeur("Ambassadeur");
     }
 
     @Gegeven("^de demo is bevestigd door de ambassadeur$")
-    public void de_demo_is_bevestigd_door_de_ambassadeur() {
+    public void de_demo_is_bevestigd_door_de_ambassadeur()
+    {
         demo.setAmbassadeur("Ambassadeur");
         demo.confirmeer();
     }
 
     @Als("^ik een ambassadeur selecteer voor de demo$")
-    public void ik_selecteer_een_ambassadeur_voor_de_demo() {
+    public void ik_selecteer_een_ambassadeur_voor_de_demo()
+    {
+
         demo.setAmbassadeur("Ambassadeur");
     }
 
     @Als("^de ambassadeur bevestigt dat hij de demo gaat geven$")
-    public void de_ambassadeur_bevestigt_dat_hij_de_demo_gaat_geven() {
+    public void de_ambassadeur_bevestigt_dat_hij_de_demo_gaat_geven()
+    {
         demo.confirmeer();
     }
 
     @Als("^de ambassadeur weigert de demo te geven$")
-    public void de_ambassadeur_weigert_de_demo_te_geven() {
+    public void de_ambassadeur_weigert_de_demo_te_geven()
+    {
         demo.weiger();
     }
 
     @Als("^de demo wordt uitgevoerd$")
-    public void de_demo_wordt_uitgevoerd() {
+    public void de_demo_wordt_uitgevoerd()
+    {
         demo.voerUit();
     }
 
